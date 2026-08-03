e21609feaf0f47cfe219936dcfb7cd32bdfb9643
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2026-08-03 14:15:45 +0700

    Add PushPin icon import to StartMenuTiles.kt

--- feature/startmenu/src/main/java/com/siroha/feature/startmenu/components/StartMenuTiles.kt
+++ feature/startmenu/src/main/java/com/siroha/feature/startmenu/components/StartMenuTiles.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PushPin
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
