4fb4d043bd744bc58b98f2b5c23c508c651a870c
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2026-08-05 15:45:12 +0700

    Upload file via GitHub Manager

--- feature/desktop/src/main/java/com/siroha/feature/desktop/DesktopViewModel.kt
+++ feature/desktop/src/main/java/com/siroha/feature/desktop/DesktopViewModel.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
