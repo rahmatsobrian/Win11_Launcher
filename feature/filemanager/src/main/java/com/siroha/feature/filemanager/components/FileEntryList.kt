6145b170885b2f7d6317b0f22afbeba43faa3bb9
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2026-08-07 09:12:42 +0700

    Upload file via GitHub Manager

--- feature/filemanager/src/main/java/com/siroha/feature/filemanager/components/FileEntryList.kt
+++ feature/filemanager/src/main/java/com/siroha/feature/filemanager/components/FileEntryList.kt
@@ -1,3 +1,5 @@
+@file:OptIn(androidx.compose.foundation.ExperimentalFoundationApi::class)
+
 package com.siroha.feature.filemanager.components
 
 import androidx.compose.foundation.combinedClickable
